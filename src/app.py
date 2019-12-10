b1cadfd4b7a07a5050fdff8cf266fab1bd47ae2a
rubengura <ruben.guerrero.ramirez@gmail.com>
2019-12-10 22:58:03 +0100

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,14 @@
+__author__ = "rgr"
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_method():
+    return 'Hello, world!'
+
+
+if __name__ == '__main__':
+    app.run()
