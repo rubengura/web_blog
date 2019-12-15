e067ee7be029cc87d8243c0998be22d594828863
rubengura <ruben.guerrero.ramirez@gmail.com>
2019-12-15 23:10:59 +0100

    html base and login, css and user added

--- src/app.py
+++ src/app.py
@@ -1,13 +1,12 @@
 __author__ = "rgr"
 
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_method():
-    return 'Hello, world!'
+    return render_template("login.html")
 
 
 if __name__ == '__main__':
